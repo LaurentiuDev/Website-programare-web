ca9f6168b19074770790d65c3e33eec01d10a9b0
marian01-dev <m.vicsoreanu@yahoo.ro>
2021-05-22 17:08:33 +0300

    add login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-document.getElementById("here").addEventListener("click", myFunction);
-function myFunction() {
-    document.getElementById("here").innerHTML = "Salut Adina!";
-    
+// document.getElementById("here").addEventListener("click", myFunction);
+// function myFunction() {
+//     document.getElementById("here").innerHTML = "Salut Adina!";
+
+//   }
+
+const logi = document.getElementById("log");
+
+logi.addEventListener("click", function () {
+  const username = document.getElementById("usrname").value;
+  const password = document.getElementById("usrpass").value;
+  conectare(username, password);
+  document.getElementById("user_contact").innerHTML = username;
+  console.log(username);
+});
+
+function conectare(username, password) {
+  if (username == "andrei") {
+    if (password == "1234") {
+      window.open("contact.html");
+    }
+  } else {
+    alert("Username sau parola gresita!");
   }
+}
