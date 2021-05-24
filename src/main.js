8b180e0f9ff2b8d53b4a9c13990984d8d4645783
emil01001101 <emilmesesan@gmail.com>
2021-05-24 12:45:40 +0200

    Right popup on profile. Done.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
-function showProfile() {
-    let width = document.getElementById('top-navigation').clientWidth;
-    document.getElementById("popover-div").style.display = "block";
-    document.getElementById("popover-div").style.left = width - 204 + "px";
+/* Show/hide profile options on the top right corner */
+function getElementPosition(id) {
+    var ele = document.getElementById(id);
+    var right = 0;
+    while (ele.tagName != "BODY") {
+        right += ele.offsetLeft;
+        ele = ele.offsetParent;
+    }
+    return { right: right };
+}
+
+function showProfileOptions() {
+    let button = document.getElementById('name-button');
+    let divHide = document.getElementById('popover-div');
+    var TopRight = getElementPosition("top-navigation");
+
+    // default values
+    divHide.style.display = 'block';
+    divHide.style.right = TopRight.right + "px";
+
+    button.onclick = function() {
+        if (divHide.style.display !== 'block') {
+            divHide.style.display = 'block';
+            divHide.style.right = TopRight.right + "px";
+        } else {
+            divHide.style.display = 'none';
+        }
+    };
 
 }
+/* End of show/hide profile options on the top right corner */
+
 document.getElementById("here").addEventListener("click", myFunction);
 
 function myFunction() {
